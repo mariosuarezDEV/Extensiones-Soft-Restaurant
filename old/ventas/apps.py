c6cf74d99a7a667e3d127ac8cb6017bfa2480ff2
Luis Mario Suárez <lmcervantessuarez@gmail.com>
2025-08-28 01:33:29 -0600

    Remove legacy Django project files
    Deleted the entire 'old' directory containing legacy Django project files, including models, settings, and related scripts. This cleanup removes unused code and configuration to streamline the repository.

--- old/ventas/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class VentasConfig(AppConfig):
-    # pyrefly: ignore  # bad-override
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ventas'
